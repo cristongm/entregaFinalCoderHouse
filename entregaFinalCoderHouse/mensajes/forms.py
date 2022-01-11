e9e14cdfb26e2c8fc68c3693e4e8482bda0b6932
Cristo García <cristo.garcia@tutelkan.com>
2022-01-11 15:20:42 -0300

    Ajustes finales de estilos-a

--- entregaFinalCoderHouse/mensajes/forms.py
+++ entregaFinalCoderHouse/mensajes/forms.py
@@ -3,4 +3,4 @@ from django.contrib.auth import get_user_model
 
 
 class FormMensajes(forms.Form):
-    mensaje = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Escribe tu mensaje'}))
+    mensaje = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Escribe tu mensaje', "rows":"4"}))
